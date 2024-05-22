1211e01ebe31ded6d022a75aae065400c199f166
alzhraafatemah <123402035+alzhraafatemah@users.noreply.github.com>
2024-05-22 15:24:08 +0300

    Update task4_Fatemah Alzhraa.php

--- task4_Fatemah Alzhraa.php
+++ task4_Fatemah Alzhraa.php
@@ -1,24 +1,21 @@
 
-echo <"br">;
+
 <?php
 $grade = 95;
 
 $result = "";
 echo "<br>";
 switch($grade) { 
-    case "95";
-        $res2 = "your grade is excellent"
-        break;
     case "80";
         $res2 = "your grade is very good";
         break;
     case "70";
         $res2 = "your grade is good";
         break;
-    case "40";
-        $res2 = "you failed";
+    case "95";
+        $res2 = "your grade is excellent";
         break;
     default: 
-        $res2 = "your grade is excellent ";}
+        $res2 = "your grade is excellent";}
         echo $res2
         ?>
