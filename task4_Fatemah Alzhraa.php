6008336d3d40ca752371675ab45a3322761a312e
alzhraafatemah <123402035+alzhraafatemah@users.noreply.github.com>
2024-05-22 14:59:06 +0300

    Add files via upload

---
+++ task4_Fatemah Alzhraa.php
@@ -0,0 +1,19 @@
+<?php
+$grade = 95;
+
+$result = "";
+echo "<br>";
+switch($grade) {
+    case "80";
+        $res2 = "your grade is very good";
+        break;
+    case "70";
+        $res2 = "your grade is good";
+        break;
+    case "40";
+        $res2 = "your failed";
+        break;
+    default: 
+        $res2 = "your grade is excellent ";}
+        echo $res2
+        ?>
